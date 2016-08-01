e10990dc1387a1e0f3e4d1e7d6dcc4cfdab9587e
Joram Clervius <clervius@gmail.com>
2016-08-01 18:28:23 -0400

    good progress, need to finish frontend for admin tonightorrow

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,8 +4,12 @@ var express = require('express');
 module.exports = function(app){
 
 
-	app.get('/manager/', function(req, res){
+	app.get('/manager*', function(req, res){
 		res.render('admin');
+	});
+
+	app.get('/', function(req, res){
+		res.render('index');
 	});
 	//API routes
 	app.get('/ath/*', require('../api/athletes'));
@@ -16,4 +20,9 @@ module.exports = function(app){
 	app.post('/show/*', require('../api/competitions/shows'));
 	app.get('/federation/*', require('../api/federations'));
 	app.post('/federation/*', require('../api/federations'));
+
+	//front-end jade
+	app.get('/client/*', function(req, res){
+	    res.render('../../public/' + req.params[0]); 
+	});
 }
