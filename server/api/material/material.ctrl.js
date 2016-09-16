1a461e4a4136850d65f5f713e67cbf34423d9edb
Franckly <clervius@gmail.com>
2016-09-16 15:06:28 -0400

    sept 15 2016

--- server/api/material/material.ctrl.js
+++ server/api/material/material.ctrl.js
@@ -6,21 +6,25 @@ var record = require('../competitions/records/record.model')
 
 module.exports = function(){
 	var allAthletes,allPosts;
-
-	athlete.find().exec((err,athletes)=>{ if(!err){ console.log('found all athletes'); allAthletes = athletes; }else{ console.log('Did not find athletes'); } });
-	post.find().exec((err,posts)=>{ if(!err){ console.log('found all posts'); allPosts = posts; }else{ console.log('Did not find posts'); } });
-
+	var getUsuals = function(){
+		console.log('getting all athletes and posts');
+		athlete.find().exec((err,athletes)=>{ if(!err){ console.log('found all athletes'); allAthletes = athletes; }else{ console.log('Did not find athletes'); } });
+		post.find().exec((err,posts)=>{ if(!err){ console.log('found all posts'); allPosts = posts; }else{ console.log('Did not find posts'); } });
+	}
 	return {
 		homePage: function(req, res){
 			console.log('get home page');
+			getUsuals();
 			res.render('material/index', {athletes:allAthletes, posts:allPosts});
 		},
 		athletes: function(req, res){
 			console.log('get athletes page');
+			getUsuals();
 			res.render('material/allAthletes', {athletes:allAthletes, posts:allPosts});
 		},
 		posts: function(req, res){
 			console.log('get posts');
+			getUsuals();
 			res.render('material/posts', {athletes:allAthletes, posts:allPosts});
 		},
 		getOneAthlete: function(req, res){
@@ -33,6 +37,7 @@ module.exports = function(){
 						if(err){ console.log(err); console.log('could not get this athletes shows')}
 						else{ 
 							console.log('got the shows'); 
+
 							res.render('material/oneAth', {athletes:allAthletes, posts:allPosts, athlete:athlete, records: records});
 						 }
 					})
@@ -66,6 +71,7 @@ module.exports = function(){
 			post.findById(req.params.id, (err,post)=>{
 				if(!err){
 					console.log('found the post');
+					getUsuals();
 					res.render('material/onePost', {post:post, athletes:allAthletes, posts:allPosts});
 				}else{
 					console.log('could not find the post');
